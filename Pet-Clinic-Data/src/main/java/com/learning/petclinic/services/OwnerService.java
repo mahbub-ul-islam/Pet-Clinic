7bd23d8a4c7cb32a7f3cbff0b8a0e75828b8208a
Mahbub Ul Islam <mahbub_ul_islam@outlook.com>
2022-06-27 01:06:48 +0600

    Implement Interfaces for Services. Closes #5.

---
+++ Pet-Clinic-Data/src/main/java/com/learning/petclinic/services/OwnerService.java
@@ -0,0 +1,16 @@
+package com.learning.petclinic.services;
+
+import com.learning.petclinic.model.Owner;
+
+import java.util.Set;
+
+public interface OwnerService {
+
+    Owner findByLastName(String lastName);
+
+    Owner findById(Long id);
+
+    Owner save(Owner owner);
+
+    Set<Owner> findAll();
+}
---
+++ Pet-Clinic-Data/src/main/java/com/learning/petclinic/services/VetService.java
@@ -0,0 +1,16 @@
+package com.learning.petclinic.services;
+
+import com.learning.petclinic.model.Vet;
+
+import java.util.Set;
+
+public interface VetService {
+
+    Vet findByLastName(String lastName);
+
+    Vet findById(Long id);
+
+    Vet save(Vet vet);
+
+    Set<Vet> findAll();
+}
