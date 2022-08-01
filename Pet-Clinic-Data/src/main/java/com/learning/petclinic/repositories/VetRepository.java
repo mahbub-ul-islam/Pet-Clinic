bed01a7f141d3bd236a1211ad107ee3f61c90e0b
Mahbub Ul Islam <mahbub_ul_islam@outlook.com>
2022-08-01 11:31:46 +0600

    Added spring data jpa service for owners. closes #46

--- Pet-Clinic-Data/src/main/java/com/learning/petclinic/repositories/VetRepository.java
+++ Pet-Clinic-Data/src/main/java/com/learning/petclinic/repositories/VetRepository.java
@@ -4,4 +4,6 @@ import com.learning.petclinic.model.Vet;
 import org.springframework.data.repository.CrudRepository;
 
 public interface VetRepository extends CrudRepository<Vet, Long> {
+
+    Vet findByLastName(String lastName);
 }
